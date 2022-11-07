77d4b0913c06a8daf0fad1df1e5c291b19064a83
neon1020 <neo3111@naver.com>
2022-11-07 11:56:19 +0900

    1107

---
+++ OwnIt/src/main/java/com/teamone/ownit/service/NoticeService.java
@@ -0,0 +1,14 @@
+package com.teamone.ownit.service;
+
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.stereotype.Service;
+
+import com.teamone.ownit.mapper.NoticeMapper;
+
+@Service
+public class NoticeService {
+
+	@Autowired
+	private NoticeMapper mapper;
+	
+}
---
+++ OwnIt/src/main/java/com/teamone/ownit/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.teamone.ownit.service;
+
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.stereotype.Service;
+
+import com.teamone.ownit.mapper.ProductMapper;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductMapper mapper;
+	
+}
